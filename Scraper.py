43abcfc9d3253e121d7caa1d209fffad4b1b913f
SternGang <mkleeberg01@gmail.com>
2025-04-23 21:41:25 -0400

    addign scraper

---
+++ Scraper.py
@@ -0,0 +1,10 @@
+# for now we will return hard coded interes for a person becuase that is the esiests and a proof of the AI being able to gen password, when a better scrabe is avalabe will implement that=
+
+
+def GenInterest(candidate):
+    
+    #candidate will be a user name that will then be given some atrabute interests that will be returned to be sent to the AI tool
+    
+    interests= "horse, yellow,2"
+    return interests
+
